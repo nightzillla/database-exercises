cf1714e6b68297a87cdce370e6ab0a7feb827abf
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-07 10:53:45 -0600

    starting albums seeder

---
+++ basic_statements.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Tupac', '2Pacalypse', 1991, 0.923,'hiphop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Backstreet Boys', 'Millennium', 1999, 24,'pop');
