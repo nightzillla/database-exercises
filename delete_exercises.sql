197548c140281386ebea7f433e4173a4f28d0da4
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-07 13:52:56 -0600

    finshed delete exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+# Albums released after 1991
+SELECT * FROM albums WHERE release_date > 1991;
+# Albums with the genre 'disco'
+SELECT * FROM albums WHERE genre = 'disco';
+# Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+DELETE FROM albums WHERE artist = 'Whitney Houston';
+
+
+
+
