9e41e655e229cd0dda69eb7a91f56e62a2db8954
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-07 11:36:11 -0600

    updated albums

---
+++ albums_seeder.sql
@@ -0,0 +1,92 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 50.2,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC', 'Back in Black', 1980, 30.1,'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', 1992, 28.7,'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 28,'Progressive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits', 1976, 41.2,'Country rock');
+
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Hotel California', 1976, 31.8,'Soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Shania Twain', 'Come On Over', 1997, 30.4,'Country');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Fleetwood Mac', 'Rumours', 1977, 29.3,'Soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 22.1,'Disco');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4,'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Bad', 1987, 21.1,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 28,'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4,'Alternative rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Various artists', 'Drity Dancing', 1987, 24.1,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Celine Dion', 'Falling into You', 1996, 21.1,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Adele', '21', 2011, 27.1,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('The Beatles', '1', 2000, 23.2,'Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Metallica', 'Metallica', 1991, 22.7,'Heavy metal');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Celine Dion', 'Lets Talk About Love', 1997, 19.5,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22.9,'Reggae');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Guns N Roses', 'Appetite for Destruction', 1987, 22.8,'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bruce Springsteen', 'Born in the U.S.A', 1984, 22,'Heartland rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('ABBA', 'Gold: Greatest Hits', 1992, 21.6,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Dire Straits', 'Brothers in Arms', 1985, 21.1,'Roots rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Santana', 'Supernatural', 1999, 20.8,'Latin rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Madonna', 'The Immaculate Collection', 1990, 19.8,'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Wall', 1979, 18.9,'Progressive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('The Beatles', 'Sgt.Pepper Lonely Hearts Club Band', 1967, 18.3,'Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Dangerous', 1991, 17,'R&B');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('The Beatles', 'Abbey Road', 1969, 16.9,'Rock');
--- basic_statements.sql
+++ basic_statements.sql
@@ -1,7 +1,4 @@
 USE codeup_test_db;
 
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Tupac', '2Pacalypse', 1991, 0.923,'hiphop');
-
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Backstreet Boys', 'Millennium', 1999, 24,'pop');
+INSERT INTO albums (artist_name, album_name, release_date, sales, genre)
+VALUES ('Tupac', '2Pacalypse', 1991, 0.923,)
