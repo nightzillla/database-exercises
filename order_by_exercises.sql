96a9037bb1bce79d42e7f26553e83f37831c208f
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-08 13:44:13 -0600

    starting order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+#PART ONE
+# Modify your first query to order by first name. The first result should be Irena Pelz and the last result should be Vidya Awdeh..
+SELECT emp_no, first_name, last_name FROM employees WHERE first_name IN ('Irena') AND last_name IN ('Pelz') OR first_name IN ('Vidya') AND last_name IN ('Awdeh') ;
+# Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+#     Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+#     Update your queries for employees with 'e' in their last name to sort the results by their employee number. Make sure the employee numbers are in the correct order.
+#     Now reverse the sort order for both queries and compare results.
